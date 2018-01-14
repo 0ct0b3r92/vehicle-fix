60dc6112b4f31aa73d87385e7daab2ebcaf5f3f8
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2018-01-14 23:08:56 +0000

    Delete server.lua

--- server.lua
+++
@@ -1,8 +0,0 @@
-AddEventHandler( 'chatMessage', function( source, n, message )
-    if ( message == "/fix" ) then 
-    	CancelEvent()
-        TriggerClientEvent( 'fixVehicle', source )
-    end 
-end )
-
--- Designed by Lucifer
