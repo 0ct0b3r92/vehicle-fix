f6861e8439b2b8da7e26fec9c6783cdfc74369c6
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2018-01-20 00:27:52 +0000

    Delete server.lua

--- server.lua
+++
@@ -1,9 +0,0 @@
-AddEventHandler( 'chatMessage', function(source, n, message)
-    if (message == "/fix" ) then
-        TriggerClientEvent('drawNotification', source)	
-        Citizen.Wait(15000)
-        TriggerClientEvent('fixVehicle', source)
-    end 
-end)
-
--- Designed by Lucifer, please don't edit.
